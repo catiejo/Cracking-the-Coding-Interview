f785119dad660337ca93f20a994201e406a435ad
catiejojo <catiejo.pidel@gmail.com>
2017-04-20 01:09:02 +0200

    additional implementations for 1.1

--- chapter1.js
+++ chapter1.js
@@ -12,8 +12,37 @@ function stringIsUnique(str) {
   return true;
 }
 
+// 1.1 Using a set
+function stringIsUnique2(str) {
+  var charSet = new Set();
+  var i, len, ch;
+  for (i = 0, len = str.length; i < len; i++) {
+    charSet.add(str.charAt(i));
+  }
+  return charSet.size == str.length;
+}
+
+// 1.1 Without additional data structures
+function stringIsUnique3(str) {
+  var marker = 0;
+  var len = str.length;
+  var i, ch;
+  while (marker < len - 1) {
+    ch = str.charAt(marker);
+    for (i = marker + 1; i < len; i++) {
+      if (ch == str.charAt(i)) {
+        return false;
+      }
+    }
+    marker++;
+  }
+  return true;
+}
+
 // 1.1 Tests
 var tests = ["hello", "abc", " ", " :D ", "abc123#$% ijk)(*&^"];
 tests.forEach(function(test) {
   console.log("\"" + test + "\": " + stringIsUnique(test));
+  console.log("\"" + test + "\": " + stringIsUnique2(test));
+  console.log("\"" + test + "\": " + stringIsUnique3(test));
 });
