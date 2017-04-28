48d2d56ccd08c5e77f8b011823b72a05e36c14bc
catiejojo <catiejo.pidel@gmail.com>
2017-04-28 17:01:39 +0200

    2.5 as-written

--- chapter2.js
+++ chapter2.js
@@ -91,3 +91,55 @@ tests1.forEach(function (test, index, array) {
   var result = listsAreEqual(removeDupes(test[0]), test[1]);
   console.log(`test${index + 1}: ${result}`);
 });
+
+// 2.5
+function OneWayNode (data, next) {
+  this.data = data;
+  this.next = next;
+}
+
+OneWayNode.prototype.equals = function (node) {
+  return this.data = node.data;
+}
+
+OneWayNode.prototype.log = function(name) {
+  console.log(`Node: ${name}`);
+  console.log(`--data = ${this.data}`);
+  console.log(`--next = ${this.next}`);
+}
+
+function sumList(num1, num2) {
+  var answer = new OneWayNode("start", null);
+  var answerHead = answer;
+  var sum, carry = 0, runner1 = num1, runner2 = num2;
+  while (runner1 != null || runner2 != null || carry != 0) {
+    sum = (runner1 == null ? 0 : runner1.data) + (runner2 == null ? 0 : runner2.data) + carry;
+    answer.next = new OneWayNode(sum % 10, null);
+    answer = answer.next;
+    carry = Math.floor(sum/10.0);
+  }
+  return answerHead.next;
+}
+
+function makeOneWayNodesFromArray(a) {
+  var node;
+  var nextNode = null;
+  a.forEach(function(data) {
+    node = new OneWayNode(data, nextNode);
+    nextNode = node;
+  });
+  return node;
+}
+
+console.log("***** 2.5 *****");
+var tests5 =
+[
+  [makeOneWayNodesFromArray([7, 1, 6]), makeOneWayNodesFromArray([5, 9, 2]), makeOneWayNodesFromArray([2, 1, 9])],
+  [makeOneWayNodesFromArray([7, 1, 6]), makeOneWayNodesFromArray([5, 9, 4]), makeOneWayNodesFromArray([2, 1, 1, 1])],
+  [makeOneWayNodesFromArray([2, 1, 9]), makeOneWayNodesFromArray([0]), makeOneWayNodesFromArray([2, 1, 9])]
+]
+tests5.forEach(function (test, index, array) {
+  var answer = sumList(test[0], test[1]);
+  var result = listsAreEqual(answer, test[2]);
+  console.log(`test${index + 1}: ${result}`);
+});
