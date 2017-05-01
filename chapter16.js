5905ed5b49fe07430aba8a4cdd2d8aa282b5ea2d
catiejojo <catiejo.pidel@gmail.com>
2017-05-01 15:07:04 +0200

    16.17 corrections

--- chapter16.js
+++ chapter16.js
@@ -3,8 +3,8 @@ function maxSum(array) {
   var curSum = 0, maxSum = 0;
   array.forEach( function (num) {
     curSum += num;
-    maxSum = Math.Max(curSum, maxSum);
-    curSum = Math.Max(curSum, 0);
+    maxSum = Math.max(curSum, maxSum);
+    curSum = Math.max(curSum, 0);
   });
   return maxSum;
 }
@@ -17,7 +17,7 @@ tests =
   [[1, 2, 3, 4, 5], 15]
 ]
 
-tests.forEach( function (test) {
+tests.forEach( function (test, index, array) {
   var sum = maxSum(test[0]);
   var result = sum == test[1];
   console.log(`16.17 test${index + 1}: ${result}`);
