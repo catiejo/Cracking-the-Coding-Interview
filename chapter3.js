28b5ab2e06e5a3e2ac06b3966c6daee2dcc638f5
catiejojo <catiejo.pidel@gmail.com>
2017-05-28 13:56:31 +0200

    adding info from back of book to 3.1

--- chapter3.js
+++ chapter3.js
@@ -4,7 +4,13 @@ There are three ways I can think of to implement multiple stacks with a single a
 
 Most simply, you could just divide the array into n sections (where n is the number
 of stacks you want). Then you push elements to their associated sections. The problem
-here is that once a stack is full, you're SOL.
+here is that once a stack is full, you're SOL. The back of the book recommends a
+circular array with flexible boundaries. Eg, if the last stack is full and one of
+the other stacks isn't, you shift the elements in the next stack over by 1 (and
+reduce its capacity by one) to accommodate the other stack. However, in my mind this
+makes pushing to a full array a really expensie operation. Also, the indices are
+modulo-ed so you can extend a stack that starts in the back of the array by over-
+flowing to the beginning (hence, circuluar). 
 
 Second, you could interleave the indexes. So for example, pushing to stack 1 would
 push to indices 0, 3, 6, etc, stack 2 to indices 1, 4, 7, and stack 3 to 2, 5, 8.
